617def5cee3e44fb0c1c2c2e43e13b021d28d0dd
usuario <usuario@T20221.ds.iteso.mx>
2013-11-06 10:28:05 -0600

    El primer cambio......>_

---
+++ TestProject/src/testproject/ClassB.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject;
+
+/**
+ *
+ * @author usuario
+ */
+public class ClassB {
+    
+    public static void go(){
+        System.out.println("HOLA MEMO");
+    }
+    
+}
